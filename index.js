3bddf0a3fa3dc56b776cd815b2f5fb2a6cc93e27
Walker Le <walker.le88@gmail.com>
2022-12-14 21:38:50 -0700

    commit tree 8ff11196b2bb4c369c5a665a5a9faa7a803b2edc

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+const num1 = 62;
+const num2 = 1;
+const multiply = num1 * num2;
+
+// const random = Number.parseInt(10.5);
+const random = Math.floor(Math.random()*1000) +1;
+
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+
+const max = Math.max(3, 15, 8, 20);
